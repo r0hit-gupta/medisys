97f50459eb09d78e7f1e60f5cc0f0759caf9364e
r0hit-gupta <rohit1997gupta@gmail.com>
2016-03-29 00:18:15 +0530

    Database Works

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard', { title: 'Express' });
+	var db = req.db;
+    var collection = db.get('products');
+    collection.find({},{},function(e,docs){
+        res.render('dashboard', {
+            title : "MediSys",
+            "products" : docs
+        });
+    });
+    //res.render('dashboard', { title: 'MediSys' });
 });
 
 module.exports = router;
